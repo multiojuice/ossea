41c7b0fc0ec8c5107e0058762c590c1d5f84358a
Owen Sullivan <multiojuice@gmail.com>
2019-03-05 15:04:22 -0500

    Initial crawling commit, getting html, now need to find links

--- main.go
+++ main.go
@@ -1,8 +1,68 @@
 package main
 
-import "fmt"
+import (
+	"io/ioutil"
+	"net/http"
+	"fmt" 
+)
+
+
+type Fetcher interface {
+	// Fetch returns the body of URL and
+	// a slice of URLs found on that page.
+	Fetch(url string) (body string, urls []string, err error)
+}
+
+
+// Crawl uses fetcher to recursively crawl
+// pages starting with url, to a maximum of depth.
+func Crawl(url string, depth int, fetcher Fetcher) {
+	// TODO: Fetch URLs in parallel.
+	// TODO: Don't fetch the same URL twice.
+	// This implementation doesn't do either:
+	if depth <= 0 {
+		return
+	}
+	body, urls, err := fetcher.Fetch(url)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Printf("found: %s %q\n", url, body)
+	for _, u := range urls {
+		Crawl(u, depth-1, fetcher)
+	}
+	return
+}
+
+type sampleFetcher map[string]int
+
+func (f sampleFetcher) Fetch(url string) (string, []string, error) {
+	fmt.Printf("HTML code of %s ...\n", url)
+	resp, err := http.Get(url)
+	// handle the error if there is one
+	if err != nil {
+		return "", nil, fmt.Errorf("Error fetching")
+	}
+
+	// do this now so it won't be forgotten
+	defer resp.Body.Close()
+	// reads html as a slice of bytes
+	html, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "", nil, fmt.Errorf("Error reading")
+	}
+	// show the HTML code as a string %s
+	fmt.Printf("%s\n", html)
+	return string(html[:len(html)]), nil, nil
+}
+
+
+var fetcher = sampleFetcher{}
 
 func main() {
-	
-
+	Crawl("https://golang.org/", 4, fetcher)
 }
+
+
+
